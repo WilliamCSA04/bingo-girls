612fa082cd6ddc161c22a34cfbab42a82dad8a61
BuildTools <williamcaetano14@gmail.com>
2022-02-01 18:32:17 -0300

    :wrench: Configured ESlint and prettier

--- app/components/tier1/Card/Card.tsx
+++ app/components/tier1/Card/Card.tsx
@@ -1,5 +1,6 @@
-import { Box, BoxProps } from "@chakra-ui/react";
+import type { BoxProps } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 export default function Card(props: BoxProps) {
-    return <Box rounded="sm" boxShadow="md" {...props} />
+  return <Box rounded="sm" boxShadow="md" {...props} />;
 }
