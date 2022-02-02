a1e399f7882b83327803c9e70bc5a17d683e66ce
BuildTools <williamcaetano14@gmail.com>
2022-02-02 00:23:41 -0300

    :construction: Logic to get streamers

--- app/components/tier3/StreamerSection/StreamerSection.tsx
+++ app/components/tier3/StreamerSection/StreamerSection.tsx
@@ -1,14 +1,38 @@
 import { Flex, Heading, VisuallyHidden } from '@chakra-ui/react';
+import type { LoaderFunction } from 'remix';
+import { useLoaderData } from 'remix';
 
 import { Section } from '../../tier0';
 import { StreamerCard } from '../../tier2';
 import type { StreamerCardProps } from '../../tier2';
+import dbClient from '~/db';
 
 export type Props = {
   data: StreamerCardProps[];
 };
 
-export default function StreamerSection({ data }: Props) {
+const getStreamers = async () => {
+  await dbClient.$connect();
+  const streamers = await dbClient.streamers.findMany();
+  await dbClient.$disconnect();
+  return streamers;
+};
+
+export const loader: LoaderFunction = async () => {
+  return getStreamers();
+};
+
+type StreamersType = Awaited<ReturnType<typeof getStreamers>>;
+
+export default function StreamerSection() {
+  const streamers = useLoaderData<StreamersType>();
+  const data =
+    streamers?.map((streamer) => ({
+      name: streamer.name,
+      alt: streamer.alternative_text,
+      src: streamer.image_endpoint,
+      social: streamer.links as { link: string; text: string }[], // TODO: find out correct way to do this
+    })) || [];
   return (
     <Section>
       <VisuallyHidden>
