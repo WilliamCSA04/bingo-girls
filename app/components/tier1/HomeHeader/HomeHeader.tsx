f93a3f8f4feb85dcb7eedb628c3304e6621e01d4
BuildTools <williamcaetano14@gmail.com>
2022-02-03 03:53:39 -0300

    :sparkles: Added switch logic to change color mode

--- app/components/tier1/HomeHeader/HomeHeader.tsx
+++ app/components/tier1/HomeHeader/HomeHeader.tsx
@@ -2,18 +2,21 @@ import {
   Box,
   Flex,
   Heading,
+  Icon,
   Image,
   Switch,
   useColorMode,
   useColorModeValue,
   VisuallyHidden,
 } from '@chakra-ui/react';
+import { BsSun, BsMoon } from 'react-icons/bs';
 
 import { Header } from '../../tier0';
 
 export default function HomeHeader() {
-  const { toggleColorMode } = useColorMode();
+  const { toggleColorMode, colorMode } = useColorMode();
   const headerBg = useColorModeValue('pink.300', 'gray.900');
+  const isLightMode = colorMode === 'light';
   return (
     <Header mb="16" p={4} bg={headerBg}>
       <VisuallyHidden>
@@ -23,9 +26,16 @@ export default function HomeHeader() {
         <Box>
           <Image src="" fallbackSrc="https://via.placeholder.com/100/" alt="" boxSize="100px" />
         </Box>
-        <Box>
-          <Switch onChange={toggleColorMode} />
-        </Box>
+        <Flex alignItems="center">
+          <Icon as={BsSun} boxSize={5} />
+          <Switch
+            onChange={toggleColorMode}
+            mx={2}
+            defaultChecked={!isLightMode}
+            aria-label={`Alterar para modo ${isLightMode ? 'escuro' : 'luz'}`}
+          />
+          <Icon as={BsMoon} boxSize={5} />
+        </Flex>
       </Flex>
     </Header>
   );
