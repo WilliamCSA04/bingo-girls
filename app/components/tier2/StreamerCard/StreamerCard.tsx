f77d482e1f0e867c65a55bf7ee42e02940f59b86
BuildTools <williamcaetano14@gmail.com>
2022-02-03 01:38:58 -0300

    :lipstick: Changed links on card

--- app/components/tier2/StreamerCard/StreamerCard.tsx
+++ app/components/tier2/StreamerCard/StreamerCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Icon, Image, Link, SimpleGrid, Text, VStack } from '@chakra-ui/react';
+import { Box, Heading, HStack, Icon, Image, Link, SimpleGrid, Text, VStack } from '@chakra-ui/react';
 
 import { Card } from '../../tier1';
 import SocialIcon from '../SocialIcon';
@@ -20,12 +20,12 @@ const socialRegex = /twitch|tiktok|youtube|facebook|instagram|twitter|linktr.ee/
 function StreamerLink({ link, text }: SocialType) {
   const icon = socialRegex.exec(link)?.[0] ?? '';
   return (
-    <SimpleGrid columns={2} spacing="2">
-      <Link href={link}>
+    <HStack>
+      <Link href={link} w="100%">
         <SocialIcon socialNetwork={icon} />
         <Text>{text}</Text>
       </Link>
-    </SimpleGrid>
+    </HStack>
   );
 }
 
@@ -34,9 +34,9 @@ export default function StreamerCard({ src, alt, name, social }: Props) {
     <Card w="300px">
       <VStack>
         <Image boxSize="300px" src={src} fallbackSrc="https://via.placeholder.com/300/" alt={alt} />
-        <VStack p="5">
+        <VStack w="100%" p={5}>
           <Heading as="h3">{name}</Heading>
-          <SimpleGrid columns={2} spacing={4}>
+          <SimpleGrid columns={2} spacing={4} w="100%">
             {social.map(({ link, text }) => (
               <StreamerLink key={link} link={link} text={text} />
             ))}
