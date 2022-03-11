d0ffe8f774ed358e991f874a2a73feab8d9d3a2c
BuildTools <williamcaetano14@gmail.com>
2022-03-11 19:39:18 -0300

    :label: Created type for frame

--- app/components/tier0/Frame/Frame.tsx
+++ app/components/tier0/Frame/Frame.tsx
@@ -2,6 +2,14 @@ import { chakra } from '@chakra-ui/react';
 
 const IFrame = chakra.iframe;
 
-export default function Frame(props: typeof IFrame) {
+type FrameType = {
+  allowfullscreen?: boolean;
+  height?: string;
+  src: string;
+  width?: string;
+  parent?: string;
+};
+
+export default function Frame(props: FrameType) {
   return <IFrame {...props} />;
 }
