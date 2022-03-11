9649ff5054def146c401768fea996a648b93bc64
BuildTools <williamcaetano14@gmail.com>
2022-03-11 19:56:50 -0300

    :sparkles: Created clip component

---
+++ app/components/tier1/Clip/Clip.tsx
@@ -0,0 +1,7 @@
+import { Frame } from '~/components/tier0';
+
+type FrameType = Parameters<typeof Frame>[0];
+
+export default function Clip(props: FrameType) {
+  return <Frame height="100%" width="100%" allowfullscreen {...props} />;
+}
--- app/components/tier1/index.ts
+++ app/components/tier1/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeHeader } from './HomeHeader';
 export { default as Card } from './Card';
+export { default as Clip } from './Clip';
 export type { CardProps } from './Card';
