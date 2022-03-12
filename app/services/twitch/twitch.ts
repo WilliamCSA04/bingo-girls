4fc19c05878b9f0cfaa72618544e4b3f6c1dd18c
BuildTools <williamcaetano14@gmail.com>
2022-03-11 22:27:48 -0300

    :label: Created clip type

--- app/services/twitch/twitch.ts
+++ app/services/twitch/twitch.ts
@@ -77,6 +77,28 @@ type GetClipsParamsType = {
   broadcasterId: string;
 };
 
+export type ClipType = {
+  id: string;
+  url: string;
+  embed_url: string;
+  broadcaster_id: string;
+  broadcaster_name: string;
+  creator_id: string;
+  creator_name: string;
+  video_id: string;
+  game_id: string;
+  language: string;
+  title: string;
+  view_count: number;
+  created_at: string;
+  thumbnail_url: string;
+  duration: number;
+};
+
+export type GetClipsType = {
+  data: ClipType[];
+};
+
 export async function getClips({ broadcasterId }: GetClipsParamsType): FetchReturnType {
   const endpoint = `${BASE_URL}/clips?login=${broadcasterId}`;
   return request(endpoint);
