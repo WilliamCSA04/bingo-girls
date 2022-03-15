49d64513d62741687af2324c1af20db3a1dfd767
BuildTools <williamcaetano14@gmail.com>
2022-03-15 18:05:28 -0300

    :recycle: Remade ClipsSection with HomeSection

--- app/components/tier3/ClipsSection/ClipsSection.tsx
+++ app/components/tier3/ClipsSection/ClipsSection.tsx
@@ -1,7 +1,6 @@
-import { Flex, Heading } from '@chakra-ui/react';
 import { useLoaderData } from 'remix';
 
-import { Section } from '~/components/tier0';
+import { HomeSection } from '~/components/tier1';
 import { TwitchClip } from '~/components/tier2';
 import type { StreamersType } from '~/routes';
 
@@ -10,13 +9,10 @@ const PLAYER_URL = 'https://player.twitch.tv/';
 export default function ClipsSection() {
   const streamers = useLoaderData<StreamersType>();
   return (
-    <Section>
-      <Heading>Clipes mais vistos</Heading>
-      <Flex justify="space-around" wrap="wrap">
-        {streamers.map((streamer) => (
-          <TwitchClip key={streamer.name} src={`${PLAYER_URL}?clip=${streamer.clips[0].id}`} />
-        ))}
-      </Flex>
-    </Section>
+    <HomeSection title="Clipes mais vistos" isColored>
+      {streamers.map((streamer) => (
+        <TwitchClip key={streamer.name} src={`${PLAYER_URL}?clip=${streamer.clips[0].id}`} />
+      ))}
+    </HomeSection>
   );
 }
