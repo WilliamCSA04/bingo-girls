f8fe4f6b2fb1536e685340453ae35ceee14137c8
BuildTools <williamcaetano14@gmail.com>
2022-03-15 18:39:31 -0300

    :iphone: Ajusted home section

--- app/components/tier1/HomeSection/HomeSection.tsx
+++ app/components/tier1/HomeSection/HomeSection.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, useColorModeValue } from '@chakra-ui/react';
+import { Heading, Stack, useColorModeValue } from '@chakra-ui/react';
 
 import { Section } from '../../tier0';
 
@@ -11,11 +11,16 @@ type PropsType = {
 export default function HomeSection({ children, title, isColored }: PropsType) {
   const bg = useColorModeValue('pink.500', 'gray.700');
   return (
-    <Section color="white" bg={isColored ? bg : 'transparent'} mt="16" p="12">
+    <Section color="white" bg={isColored ? bg : 'transparent'} mt="16" px={[0, '12']} py="12">
       <Heading mb="8">{title}</Heading>
-      <Flex justify="space-between" wrap="wrap">
+      <Stack
+        direction={{ base: 'column', xl: 'row' }}
+        align="center"
+        justify={{ xl: 'space-between' }}
+        wrap="wrap"
+      >
         {children}
-      </Flex>
+      </Stack>
     </Section>
   );
 }
--- app/components/tier3/ClipsSection/ClipsSection.tsx
+++ app/components/tier3/ClipsSection/ClipsSection.tsx
@@ -1,3 +1,4 @@
+import { useBreakpointValue } from '@chakra-ui/react';
 import { useLoaderData } from 'remix';
 
 import { HomeSection } from '~/components/tier1';
@@ -8,10 +9,17 @@ const PLAYER_URL = 'https://player.twitch.tv/';
 
 export default function ClipsSection() {
   const streamers = useLoaderData<StreamersType>();
+  const height = useBreakpointValue({ base: '200px', md: '300px' });
+  const width = useBreakpointValue({ base: '300px', md: '400px' });
   return (
     <HomeSection title="Clipes mais vistos" isColored>
       {streamers.map((streamer) => (
-        <TwitchClip key={streamer.name} src={`${PLAYER_URL}?clip=${streamer.clips[0].id}`} />
+        <TwitchClip
+          key={streamer.name}
+          src={`${PLAYER_URL}?clip=${streamer.clips[0].id}`}
+          width={width}
+          height={height}
+        />
       ))}
     </HomeSection>
   );
